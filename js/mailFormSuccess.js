48611f29f11fa3d4bac139bc6069f34d155c8c6b
shireesha <shireesha.p@stayzilla.com>
2016-05-01 22:28:03 +0530

    Final touch up1

--- js/mailFormSuccess.js
+++
@@ -1,6 +0,0 @@
-$('.mail-form').submit(function(e) {
-    $('#messages').removeClass('hide').addClass('alert alert-success alert-dismissible').slideDown().show();
-    $('#messages_content ,#messages').html('<p><span class="custom-icon glyphicon glyphicon-ok"></span>Message sent ! Thanks for reaching out! Someone from our team will get back to you soon</p>').delay(1200).addClass("in").fadeOut(4500);
-    e.preventDefault();
-});
-
--- js/widget.js
+++ js/widget.js
@@ -16,4 +16,12 @@ $(document).ready(function(){
     $('.help-btn').click(function() {
         $('.widget-container').toggle();
     });
+
+    $('.mail-form').submit(function(e) {
+        $('#messages').removeClass('hide').addClass('alert alert-success alert-dismissible').slideDown().show();
+        $('#messages_content ,#messages').html('<p><span class="custom-icon glyphicon glyphicon-ok"></span>Message sent ! Thanks for reaching out! Someone from our team will get back to you soon</p>').delay(1200).addClass("in").fadeOut(4500);
+        e.preventDefault();
+    });
 });
+
+
